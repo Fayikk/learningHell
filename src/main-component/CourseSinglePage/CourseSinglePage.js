480863ab4de770d810ba135c04d26373865e5f14
Fayikk <veznedaroglufayik2@gmail.com>
2024-06-22 14:59:02 +0300

    commit_sideBar_updated

--- src/main-component/CourseSinglePage/CourseSinglePage.js
+++ src/main-component/CourseSinglePage/CourseSinglePage.js
@@ -8,16 +8,40 @@ import CoureseTab from './Tabs/CoureseTab';
 import Sidebar from './sidebar';
 import { useGetCourseDetailByIdQuery } from '../../api/courseApi';
 import IsLoading from '../../components/Loading/IsLoading';
-
+import { useThisCourseEnrolledUserMutation } from '../../api/studentCourseApi';
+import { useSelector } from 'react-redux';
 const CourseSinglePage = (props) => {
     const { slug } = useParams()
     const {data,isLoading} = useGetCourseDetailByIdQuery(slug);
+    const [CheckHasThisCourse] = useThisCourseEnrolledUserMutation();
+    const [ownMyCourse,setOwnMyCourse] = useState(false);
+    const [isEnrolledCourse,setIsEnrolledCourse] = useState(false);
     const [course,setCourse] = useState()
+    const authenticationState = useSelector((state) => state.authStore.nameIdentifier);
+
     useEffect(()=>{
         if (data) {
-            
+            console.log("trigger",data.result.courseId)
             setCourse(data.result)
         }
+
+        async function CheckActiveCourse(){
+            const model = {
+                userId:authenticationState,
+                courseId:data.result.courseId 
+            }
+            // if (user.id === useSelector((state) => state.authStore)) {
+            //     setOwnMyCourse(true)
+            // }
+
+
+             await  CheckHasThisCourse(model).then((response) =>setIsEnrolledCourse(response.data))
+            
+            //  
+          }
+            CheckActiveCourse();
+
+
     },[isLoading])
     
     if (isLoading || !course ) {
@@ -42,7 +66,12 @@ const CourseSinglePage = (props) => {
                                 <CoureseTab CoursesDetails={course} />
                             </div>
                         </div>
+                        {
+                            !isEnrolledCourse ? (
                         <Sidebar CourseDetail = {course} />
+
+                            ) : ("")
+                        }
                     </div>
                 </div>
             </div>
