3c6cf3433b96827e190d44e9e0b1a7f76e64a8b4
Fayikk <veznedaroglufayik2@gmail.com>
2024-10-02 21:30:22 +0300

    commit

--- src/api/Base/baseApiModel.js
+++ src/api/Base/baseApiModel.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "http://www.learninghell.com:81/api/";
-export const baseUrl = "https://localhost:7042/api/"
+export const baseUrl = "http://www.learninghell.com:81/api/";
+// export const baseUrl = "https://localhost:7042/api/"
