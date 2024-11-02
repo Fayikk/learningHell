baed877c68f9436cc900fce9bf711964e20daecf
Fayikk <veznedaroglufayik2@gmail.com>
2024-11-02 23:07:43 +0300

    dd

--- src/main-component/CourseSinglePage/CourseSinglePage.js
+++ src/main-component/CourseSinglePage/CourseSinglePage.js
@@ -32,6 +32,10 @@ const CourseSinglePage = () => {
 
   useEffect(() => {
     if (data) {
+
+      console.log("trigger data",data)
+
+
       setCourse(data.result.item1);
     }
 
@@ -70,27 +74,31 @@ const CourseSinglePage = () => {
             },
           ]}
         />
-        <div className="flex justify-between bg-gray-200 p-5 rounded-2xl sm:flex-row flex-col gap-4">
-          <img
-            src={courseData?.courseImage}
-            alt=""
-            className="rounded-md shadow-md sm:max-w-[550px] h-auto flex order-1"
-          />
-          <div className=" w-full gap-2 flex flex-col justify-center gap-2 px-1">
-            <h1 className="text-2xl font-bold">{courseData?.courseName}</h1>
-            <p className="text-black">{courseData?.courseDescription}</p>
+<div className="flex justify-between bg-gray-200 p-5 rounded-2xl sm:flex-row flex-col gap-4">
+  <video
+    src={courseData?.introductionVideoUrl}
+    poster={courseData?.courseImage} 
+    className="rounded-md shadow-md sm:max-w-[550px] h-auto flex order-1 no-outline"
+    controls
+    onClick={(e) => e.target.play()}
+  />
+  <div className="w-full gap-2 flex flex-col justify-center gap-2 px-1">
+    <h1 className="text-2xl font-bold">{courseData?.courseName}</h1>
+    <p className="text-black">{courseData?.courseDescription}</p>
 
-            {isEnrolledCourse ? (
-              <div className="flex justify-end">
-                <button className="theme-btn-s2" onClick={triggerButton}>
-                  Derse Başla
-                </button>
-              </div>
-            ) : (
-              <Sidebar courseDetail={courseData}></Sidebar>
-            )}
-          </div>
-        </div>
+    {isEnrolledCourse ? (
+      <div className="flex justify-end">
+        <button className="theme-btn-s2" onClick={triggerButton}>
+          Derse Başla
+        </button>
+      </div>
+    ) : (
+      <Sidebar courseDetail={courseData}></Sidebar>
+    )}
+  </div>
+</div>
+
+
         <div className="flex flex-col sm:flex-row gap-3 sm:gap-10 items-start">
           <div className="flex sm:flex-col flex-1 flex-col justify-between sm:gap-8 gap-4">
             <CourseSingleAccardion courseDetail={course} />
